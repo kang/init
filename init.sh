ab81d9cdad38f93b019b43f7d80cacdb6e701ef3
Brian Kang <kang@users.noreply.github.com>
2016-03-12 00:41:08 -0800

    remove clutter

--- init.sh
+++ init.sh
@@ -2,90 +2,26 @@ fancy_echo() {
   printf "\n%b\n" "$1"
 }
 
-install_if_needed() {
-  local package="$1"
-
-  if [ $(dpkg-query -W -f='${Status}' "$package" 2>/dev/null | grep -c "ok installed") -eq 0 ];
-  then
-    sudo aptitude install -y "$package";
-  fi
-}
-
-append_to_zshrc() {
-  local text="$1" zshrc
-  local skip_new_line="$2"
-
-  if [[ -w "$HOME/.zshrc.local" ]]; then
-    zshrc="$HOME/.zshrc.local"
-  else
-    zshrc="$HOME/.zshrc"
-  fi
-
-  if ! grep -Fqs "$text" "$zshrc"; then
-    if (( skip_new_line )); then
-      printf "%s\n" "$text" >> "$zshrc"
-    else
-      printf "\n%s\n" "$text" >> "$zshrc"
-    fi
-  fi
-}
-
-#!/usr/bin/env bash
-
-trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
-set -e
-
-if [[ ! -d "$HOME/.bin/" ]]; then
-  mkdir "$HOME/.bin"
-fi
-
-if [ ! -f "$HOME/.zshrc" ]; then
-  touch "$HOME/.zshrc"
-fi
-
-append_to_zshrc 'export PATH="$HOME/.bin:$PATH"'
-
-if ! grep -qiE 'wheezy|jessie|precise|trusty' /etc/os-release; then
-  fancy_echo "Sorry! we don't currently support that distro."
-  exit 1
-fi
-
-fancy_echo "Updating system packages ..."
-  if command -v aptitude >/dev/null; then
-    fancy_echo "Using aptitude ..."
-  else
-    fancy_echo "Installing aptitude ..."
-    sudo apt-get install -y aptitude
-  fi
-
-  sudo aptitude update
-
 fancy_echo "Installing git, for source control management ..."
-  install_if_needed git
-
-fancy_echo "Installing ctags, to index files for vim tab completion of methods, classes, variables ..."
-  install_if_needed exuberant-ctags
+  sudo apt-get install git
 
 fancy_echo "Installing vim ..."
-  install_if_needed vim-gtk
+  sudo apt-get install vim
 
 fancy_echo "Installing tmux, to save project state and switch between projects ..."
-  install_if_needed tmux
+  sudo apt-get install tmux
 
 fancy_echo "Installing mosh, an ssh replacement ..."
-  install_if_needed mosh
-
-fancy_echo "Installing watch, to execute a program periodically and show the output ..."
-  install_if_needed watch
+  sudo apt-get install mosh
 
 fancy_echo "Installing curl ..."
-  install_if_needed curl
+  sudo apt-get install curl
 
 fancy_echo "Installing zsh ..."
-  install_if_needed zsh
+  sudo apt-get install zsh
 
 fancy_echo "Installing node ..."
-  install_if_needed nodejs
+  sudo apt-get install nodejs
 
 fancy_echo "Changing your shell to zsh ..."
   chsh -s $(which zsh)
